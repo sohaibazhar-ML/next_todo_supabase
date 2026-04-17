cbd0729470a18904744136816947cbd7c2b5cb4d
sohaibazhar-ML <sohaib.azhar@maqsoodlabs.com>
2026-04-17 16:33:50 +0500

    feat: add flagcdn.com to allowed image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
